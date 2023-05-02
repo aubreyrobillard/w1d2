80623d148c6e1179dfb63643b79aaa2a50d813ef
Aubrey Robillard <aubreyrobillard@Aubreys-MacBook-Pro.local>
2023-05-01 22:21:33 -0500

    finished loop practice

---
+++ loops.js
@@ -0,0 +1,27 @@
+//Loop-D-Loops
+
+// //1. Create a loop that logs the numbers from 0-99 (ascending)
+// for (let i = 0; i < 100; i++)
+//     console.log(i)
+
+//2. Create a loop that logs the numbers from 99-0 (descending)
+// for (let i = 99; i>=0; i--)
+//     console.log(i)
+
+//3. Create a loop that logs the EVEN numbers from 0-98 (ascending)
+// for(let i = 0; i < 100; i++){
+//     if(i % 2 === 0){
+//         console.log(i)
+//     }
+// }
+
+//4. Create a loop that logs the EVEN numbers from 98-0 (descending)
+// for(let i = 98; i >= 0; i--){
+//         if(i % 2 === 0){
+//             console.log(i)
+//         }
+//     }
+
+//7. Create a loop that logs the numbers from 49-72 (ascending)
+for(let i = 49; i <= 72; i++)
+    console.log(i)
